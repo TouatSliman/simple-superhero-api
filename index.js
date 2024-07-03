06d7f27ea81c871574418a583d5cdb0781cb84ec
sliman <ssoverlord04@gmail.com>
2024-07-03 21:06:30 +0100

    change env variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4040;
+require("dotenv").config();
+const PORT = process.env.PORT ;
 
 app.use(express.json());
 
